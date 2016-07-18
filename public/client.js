942b8ac44536380479a1d7fcc41639e7bd02c734
Kris Foss <kris.foss@gmail.com>
2016-07-18 17:22:15 -0600

    started chart

--- public/client.js
+++ public/client.js
@@ -1,3 +1,6 @@
+var Chart = require('src/chart.js')
+// let myChart = new Chart({...})
+
 var socket = io();
 
 var connectionCount = document.getElementById('connection-count');
@@ -21,8 +24,22 @@ for (var i = 0; i < buttons.length; i++) {
   });
 }
 /////////////////////////////////
+var totalVotes = document.getElementById('totals');
+
+var votesA = document.getElementById('a-votes');
+var votesB = document.getElementById('b-votes');
+var votesC = document.getElementById('c-votes');
+var votesD = document.getElementById('d-votes');
+
 socket.on('voteCount', function (votes) {
   console.log(votes);
+  votesA.innerText = votes["A"];
+  votesB.innerText = votes["B"];
+  votesC.innerText = votes["C"];
+  votesD.innerText = votes["D"];
+  // debugger;
+  // totals.innerHTML = votes;
+  votesGraph(votesA,votesB,votesC,votesD);
 });
 
 //////////////////////////////////
@@ -32,3 +49,43 @@ socket.on('voteMessage', function (message) {
   console.log(message);
   yourVotesMessage.innerHTML = message;
 });
+///////////////////////////////////
+
+function votesGraph(A,B,C,D){
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [A,B,C,D],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });}
